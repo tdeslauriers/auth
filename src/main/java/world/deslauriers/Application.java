f4fdbbd9f296575803a1e1c608637a9d4d45b711
tdeslauriers <thomas.deslauriers@gmail.com>
2022-05-25 07:00:15 -0500

    fixing k8s

--- src/main/java/world/deslauriers/Application.java
+++ src/main/java/world/deslauriers/Application.java
@@ -4,23 +4,25 @@ import io.dekorate.docker.annotation.DockerBuild;
 import io.dekorate.kubernetes.annotation.*;
 import io.micronaut.runtime.Micronaut;
 
+
 @KubernetesApplication(
-    name = "auth",
-    serviceType = ServiceType.NodePort,
-    expose = true,
-    host = "localhost",
-    replicas = 3,
-    envVars = {
-        @Env(name = "AUTH_JDBC_URL", configmap = "auth-svc-config", value = "jdbc_url"),
-        @Env(name = "AUTH_JDBC_DRIVER", configmap = "auth-svc-config", value = "jdbc_driver"),
-        @Env(name = "AUTH_JDBC_DIALECT", configmap = "auth-svc-config", value = "jdbc_dialect"),
-        @Env(name = "CORS_URL", configmap = "auth-svc-config", value = "cors_url"),
-        @Env(name = "AUTH_JDBC_USERNAME", secret = "auth-jdbc", value = "username"),
-        @Env(name = "AUTH_JDBC_PASSWORD", secret = "auth-jdbc", value = "password"),
-        @Env(name = "JWT_GENERATOR_SIGNATURE_SECRET", secret = "jwt", value = "signature-pw")},
-    imagePullPolicy = ImagePullPolicy.Always,
-    labels = @Label(key = "app", value = "auth"),
-    ports = @Port(name = "http", containerPort = 8080)
+        name = "auth",
+        serviceType = ServiceType.NodePort,
+        expose = true,
+        host = "localhost",
+        replicas = 3,
+        imagePullPolicy = ImagePullPolicy.Always,
+        labels = @Label(key = "app", value = "auth"),
+        ports = @Port(name = "http", containerPort = 8080),
+        envVars = {
+                @Env(name = "AUTH_JDBC_URL", configmap = "auth-svc-config", value = "jdbc_url"),
+                @Env(name = "AUTH_JDBC_USERNAME", configmap = "auth-svc-config", value = "jdbc_username"),
+                @Env(name = "AUTH_JDBC_DRIVER", configmap = "auth-svc-config", value = "jdbc_driver"),
+                @Env(name = "AUTH_JDBC_DIALECT", configmap = "auth-svc-config", value = "jdbc_dialect"),
+                @Env(name = "CORS_URL", configmap = "auth-svc-config", value = "cors_url"),
+                @Env(name = "AUTH_JDBC_PASSWORD", secret = "auth-test-mariadb-galera", value = "mariadb-password"),
+                @Env(name = "JWT_GENERATOR_SIGNATURE_SECRET", secret = "jwt", value = "signature-pw")
+        }
 )
 @DockerBuild(group = "tdeslauriers", name = "auth")
 public class Application {
